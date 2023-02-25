a69e6819a193d5731342e3b0aa0a60bf31e95c20
Anna <shiskanovaag@mail.ru>
2023-02-25 21:09:49 +1000

    Classroom

--- Classroom/Program.cs
+++ Classroom/Program.cs
@@ -1,4 +1,41 @@
-﻿static void Task31()
+﻿Console.Clear();
+Console.WriteLine("1 - Task 31");
+Console.WriteLine("2 - Task 32");
+Console.WriteLine("3 - Task 33");
+int NumberOfTask = Prompt("Введите номер задачи");
+switch(NumberOfTask)
+{
+    case 1:
+    Console.Clear();
+    Task31();
+    break;
+
+    case 2:
+    Console.Clear();
+    Task32();
+    break;
+
+    case 3:
+    Console.Clear();
+    Task33();
+    break;
+
+    default:
+    Console.WriteLine("Номер задачи введен неверно");
+    break;
+    
+}
+
+static int Prompt(string massage)//метод запроса числа
+{
+    Console.WriteLine(massage);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+
+
+static void Task31()
 {
     //Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
     // Найдите сумму отрицательных и положительных элементов массива.
@@ -28,7 +65,7 @@
 
     void PrintResult(int[] array, int positive, int negative)
     {
-    Console.WriteLine($"Array result is {String.Join("|", array)}");//Join помогает вывести массив в читаемой форме через разделитель, здесь разделитель |//
+    Console.WriteLine($"Array result is {String.Join("|", array)}");//Join помогает вывести массив в читаемой форме через разделитель, здесь разделитель 
     Console.WriteLine($"Positive sum is {positive} and Negative sum is {negative}");
     }
 
@@ -36,4 +73,89 @@
     SumArray(arrayResult);
     PrintResult(arrayResult, positive, negative);
 }
-Task31();
+
+
+static void Task32()
+{
+//     // напишите программу замены элементов массива: положительные элементы замените на отрицательные и наоборот
+
+    int[] array = new int[8];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-10,11);
+        Console.Write($"{array[i]}\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+        Console.Write($"{array[i]}\t");
+    }
+}
+
+
+static void Task33()
+{
+    //Задача 33: Задайте массив. Напишите программу, которая определяет, 
+    //присутствует ли заданное число в массиве.
+    //4; массив [6, 7, 19, 345, 3] -> нет
+    //-3; массив [6, 7, 19, 345, -3] -> да
+    
+
+    
+        int[] array = new int[8];
+        Random random = new Random();
+        
+       
+       
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(-10,11);
+            Console.Write($"{array[i]}\t");
+        }
+
+        int a = GetNumber();
+
+    
+
+        int GetNumber()
+        {
+            Console.Write("Input number ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            return number;
+            
+        }
+        
+
+        static bool TrueFalse(int[] array, int a)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == a)
+                return true;
+            }
+            return false;
+            
+         
+
+        }
+        if (TrueFalse(array, a))
+        {
+            System.Console.WriteLine($"Number {a} is found");
+
+        }
+        else
+        {
+            System.Console.WriteLine("Number is not found");
+
+        }
+        
+            
+       
+
+
+
+
+}
