60529dbfa1a96c7a6f2cdedac58d7509067ea58c
Anna <shiskanovaag@mail.ru>
2023-02-27 14:51:11 +1000

    Task36

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -12,7 +12,7 @@ switch(NumberOfTask)
 
     case 2:
     Console.Clear();
-    // Task36();
+    Task36();
     break;
 
     case 3:
@@ -39,7 +39,7 @@ static void Task34()
 
     Console.WriteLine("Input lenght array");
     int lenght = Convert.ToInt32(Console.ReadLine());
-    int[] array = new int[lenght];//for exemple 10 elements
+    int[] array = new int[lenght];
     
     int count = 0;
 
@@ -79,6 +79,59 @@ static void Task34()
     array = RandomNumbersArray();
     CountEvenNumbers(array);
     PrintResult(array, count);
+}
+
+
+static void Task36()
+{ //Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+    Console.WriteLine("Input lenght array");
+    int lenght = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[lenght];
+    int sum = 0;
+
+     int [] RandomNumbersArray()
+    {   
+        int [] arr = new int[lenght];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-100,101);
+    }
+    return arr;
+    }
+
+    void SumOddNumbers(int[] array)
+    {
+        foreach (int el in array)
+     {
+        if (el % 2 != 0)
+        {
+           sum += el;
+        }
+
+     }
+   }
+
+   void PrintResult(int[] arr, int sum)
+    {
+    Console.WriteLine($"Array result is {String.Join(" ", array)}");//Join помогает вывести массив в читаемой форме через разделитель, здесь разделитель 
+    Console.Write($"Sum odd {sum}");
+    
+    }
+    
+
+
+
+
+
+
+
+
+    array = RandomNumbersArray();  
+    SumOddNumbers(array);  
+    PrintResult(array, sum);
 }
 
 
